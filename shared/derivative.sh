48326775e77ae1dce16d81b954c1e2a117f643c2
Lucien van Wouw <lwo@iisg.nl>
2012-06-08 00:30:39 +0200

    Exit values are byte types. So we return a 255 for indicating a non service task

--- shared/derivative.sh
+++ shared/derivative.sh
@@ -23,5 +23,5 @@ if [ -f "$file" ] ; then
 else
     echo "We cannot make a derivative of type $derivative"
     echo "This is not an error. Just there is no such service."
-    exit 799
+    exit 255
 fi
