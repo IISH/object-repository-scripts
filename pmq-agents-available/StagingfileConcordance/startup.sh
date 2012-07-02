c8b26de5ef3ab81b653c621a85be66e93d85ec33
Lucien van Wouw <lwo@iisg.nl>
2012-07-02 17:43:13 +0200

    Startup for validation

--- pmq-agents-available/StagingfileConcordance/startup.sh
+++ pmq-agents-available/StagingfileConcordance/startup.sh
@@ -6,4 +6,14 @@
 # Add Instruction
 # Prepare a mets document
 
+scripts=$scripts
+validation=$validation
+
 source $scripts/shared/parameters.sh
+fileSet=$fileSet
+na=$na
+
+//java -cp $validation org.objectrepository.validation.ConcordanceMain -dataDir /mnt/sa/12345/testuser2/ARCH00040/ -prefix ARCH00040 -pidprefix 10622
+
+prefix=$(dirname $fileSet)
+java -cp $validation org.objectrepository.validation.ConcordanceMain -dataDir $fileSet -prefix $prefix -pidprefix $na
