2d3164599e368d7708e99c478439628382f145ed
Lucien van Wouw <lwo@iisg.nl>
2012-07-25 11:16:52 +0200

    Some comments

--- shared/limit_connections.sh
+++ shared/limit_connections.sh
@@ -3,5 +3,6 @@
 n=$(lsof | grep mongod | grep TCP | wc)
 tcp=0$(echo $n | rev | cut -d" " -f1)
 if [ $tcp -gt 10000 ] ; then
+    echo "TCP connection number of $tcp is rather large. Restarting mongos..."
 	service mongos restart
 fi
