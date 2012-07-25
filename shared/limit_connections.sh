dc42c7664cd66a14393a0dc32323fb2d68522ff2
Lucien van Wouw <lwo@iisg.nl>
2012-07-25 12:20:33 +0200

    Limit tcp connections to 8000

--- shared/limit_connections.sh
+++ shared/limit_connections.sh
@@ -3,7 +3,7 @@
 n=$(lsof | grep mongod | grep TCP | wc)
 tcp=0$(echo $n | rev | cut -d" " -f1)
 echo "TCP connection number is $tcp"
-if [ $tcp -gt 9000 ] ; then
+if [ $tcp -gt 8000 ] ; then
     echo "Rather large... restarting mongos..."
 	service mongos restart
 fi
