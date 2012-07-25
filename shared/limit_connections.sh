10e6a9061ec31d7cbd6c5e41a109768722bb0c2f
Lucien van Wouw <lwo@iisg.nl>
2012-07-25 13:47:31 +0200

    Disable restart mongos

--- shared/limit_connections.sh
+++ shared/limit_connections.sh
@@ -2,8 +2,6 @@
 
 n=$(lsof | grep mongo | grep TCP | wc)
 tcp=0$(echo $n | rev | cut -d" " -f1)
-echo "TCP connection number is $tcp"
-if [ $tcp -gt 8000 ] ; then
-    echo "Rather large... restarting mongos..."
-	service mongos restart
+if [ $tcp -gt 10000 ] ; then
+    echo "Warning: TCP connection number is $tcp... seems rather high"
 fi
