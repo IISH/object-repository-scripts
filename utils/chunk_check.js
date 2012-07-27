b327c1da3a682405f7d0a447b7e6a5da32fcacad
Lucien van Wouw <lwo@iisg.nl>
2012-07-27 18:52:31 +0200

    Integrity check per collection using chunk_check.js

--- utils/chunk_check.js
+++ utils/chunk_check.js
@@ -9,37 +9,31 @@
  */
 
 assert(db.getName() != 'test', "The database is the test database. Startup specifying a production database: 'mongo host/database'");
+assert(collection, "Must have collection name: [ns].files");
+var index = collection.lastIndexOf(".files");
+assert(index != -1, "Collections must be [ns].files");
 
+print("Begin check FSGrid collection " + collection);
+var namespace = collection.substring(0, index);
+var filesCollection = db.getCollection(namespace + '.files');
+var chunksCollection = db.getCollection(namespace + '.chunks');
 
-var collectionNames = db.getCollectionNames();
-var length = collectionNames.length;
-for (var i = 0; i < length; i++) {
-    var collectionName = collectionNames[i];
-    var index = collectionName.lastIndexOf(".files");
-    if (index != -1) {
-        print("Begin check FSGrid collection " + collectionName);
-        var namespace = collectionName.substring(0, index);
-        var filesCollection = db.getCollection(namespace + '.files');
-        var chunksCollection = db.getCollection(namespace + '.chunks');
-
-        var count = 0;
-        filesCollection.find().forEach(function (file) {
-            count++;
-            //print(c + ". Check document " + file._id);
-            var nc = Math.ceil(file.length / file.chunkSize);
-            for (var n = 0; n <= nc; n++) {
-                var chunk = chunksCollection.findOne({files_id:file._id, n:n, data:{$exists:true}}, {data:0});
-                if (n == nc) {
-                    if (chunk) {
-                        print(file._id + " = corrupt document. There are too many chunks ! Expected " + nc + " but got " + n);
-                    }
-                } else {
-                    if (!chunk) {
-                        print(file._id + " = corrupt document. Chunks are missing! n=" + n);
-                    }
-                }
+var count = 0;
+filesCollection.find().forEach(function (file) {
+    count++;
+    //print(c + ". Check document " + file._id);
+    var nc = Math.ceil(file.length / file.chunkSize);
+    for (var n = 0; n <= nc; n++) {
+        var chunk = chunksCollection.findOne({files_id:file._id, n:n, data:{$exists:true}}, {data:0});
+        if (n == nc) {
+            if (chunk) {
+                print(file._id + " = corrupt document. There are too many chunks ! Expected " + nc + " but got " + n);
             }
-        });
-        print("Checked documents: " + count);
+        } else {
+            if (!chunk) {
+                print(file._id + " = corrupt document. Chunks are missing! n=" + n);
+            }
+        }
     }
-}
+});
+print("Checked documents: " + count);
