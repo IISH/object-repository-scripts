efea6e98ecfc9adf87c13b1562b2c9d32b13b0d0
Lucien van Wouw <lwo@iisg.nl>
2012-10-05 15:17:47 +0200

    Remove documents when desired added.

--- shared/delete.sh
+++ shared/delete.sh
@@ -7,22 +7,29 @@ pid=$pid
 
 for bucket in "master" "level1" "level2" "level3"
 do
-    files_id=$(mongo $db --quiet --eval "db.$bucket.files.findOne({'metadata.pid':$pid}, {_id:1})._id")
-    mongo $db --quiet --eval "db.$bucket.files.remove(ObjectId('$files_id'))"
+    files_id=$(mongo $db --quiet --eval "var doc=db.$bucket.files.findOne({'metadata.pid':'$pid'}, {_id:1});if ( doc ){print(doc._id)}")
+    if [ -z $files_id ] ; then
+        echo "PID $pid not in database."
+        exit -1
+    fi
+
+    mongo $db --quiet --eval "db.$bucket.files.remove({_id:'$files_id'})"
     mongo $db --quiet --eval "db.$bucket.chunks.remove({files_id:'$files_id'})"
 
     # Verify our removal
-    count=$(mongo $db --quiet --eval "db.$bucket.files.count({_id:ObjectId('$files_id')})")
-    if [$count != 0] ; then
+    count=$(mongo $db --quiet --eval "db.$bucket.files.count({_id:'$files_id'})")
+    if [ $count != 0 ] ; then
         echo "Failed to delete document $pid in files.$bucket"
         exit -1
     fi
 
     count=$(mongo $db --quiet --eval "db.$bucket.chunks.count({files_id:'$files_id'})")
-    if [$count != 0] ; then
+    if [ $count != 0 ] ; then
         echo "Failed to delete document $pid in chunks.$bucket"
         exit -1
     fi
 done
+
+echo "Document $pid removed"
 
 exit $?
