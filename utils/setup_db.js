4e486f21485a1ab7d0da5bda781eb3940bf7bfbd
Lucien van Wouw <lwo@iisg.nl>
2012-10-07 19:31:08 +0200

    Prepare utility for shard key

--- utils/setup_db.js
+++ utils/setup_db.js
@@ -21,14 +21,17 @@ admin.runCommand("flushRouterConfig");
 admin.runCommand({ enablesharding:db.getName() });
 
 // The shard key is a integer of 32 bit over three parts
-var keys = ['555555555555555555555555555555555555555555555555', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'];
+// shard 0: [-2147483648, -715827884] = 1431655765
+// shard 1: [-715827883, 715827882]   = 1431655765
+// shard 2: [715827883, 2147483647]   = 1431655765
+var keys = [-715827883, 715827882];
 var buckets = ['master', 'level1', 'level2', 'level3'];
 for (var i = 0; i < buckets.length; i++) {
     var collFiles = buckets[i] + ".files";
     var collChunks = buckets[i] + ".chunks";
 
     // Add index
-    db.getCollection(collChunks).ensureIndex({files_id:1}, {unique:false});
+    db.getCollection(collChunks).ensureIndex({files_id:1, n:1}, {unique:true});
     db.getCollection(collFiles).ensureIndex({'metadata.pid':1}, {unique:true});
 
     // Shard collection
@@ -38,7 +41,6 @@ for (var i = 0; i < buckets.length; i++) {
     // Pre splitting the chunks over the shard. For each shard we have a key range: shards=[n]; keys=[n-1].
     for (var k = 0; k < keys.length; k++) {
         var key = keys[k];
-        assert(key.length == 48, "Key's " + key + " radix must be 24");
         print("split collection " + shardThis + " with shardkey " + key);
         admin.runCommand({ split:shardThis, middle:{ files_id:key} });
     }
--- utils/shardkey.sh
+++ utils/shardkey.sh
@@ -2,17 +2,31 @@
 
 scripts=$scripts
 
+# First create a list of all identifiers per database and per collection.
+# Count the numbers per shard. These ought to be exclusive as there is no cardinality for the shardkey 'files_id'
+# Calculate the new key per shard: a 32-bit integer: [-2147483648, 2147483647]
+# shard 0: [-2147483648, -715827884] = 1431655765
+# shard 1: [-715827883, 715827882]   = 1431655765
+# shard 2: [715827883, 2147483647]   = 1431655765
 for db in or_10622 or_10798 or_10848 or_10851 or_10891
 do
     for ns in level3 level2 level1 master
     do
-        count=0
-        for shard in "0" "2" "4"
+        file=$db.$ns.files.txt
+        echo "Collection list $db.$ns.files into $file"
+        mongo $db --quiet --eval "db.$ns.files.find({},{_id:1}).forEach(function(d){print(d._id)})" > $file
+        chunks=$db.$ns.chunks.txt
+        echo "database collection files_id rosaluxemburg0 rosaluxemburg2 rosaluxemburg4" > $chunks
+        for files_id in $(cat $file)
         do
-            echo "Collection $db.$ns"
-            mongo rosaluxemburg$shard $db --quiet --eval "var shard='$shard';var ns='$ns';var count=$count;var index=0;" $scripts/utils/shardkey.sh
-            inc=$(mongo $db --quiet --eval "db.$ns.files.count()")
-            count=$(count+$inc)
+            rosaluxemburg0=$(mongo rosaluxemburg0:27018/$db --quiet --eval "db.$ns.chunks.count({files_id:'$files_id'})")
+            rosaluxemburg2=$(mongo rosaluxemburg2:27018/$db --quiet --eval "db.$ns.chunks.count({files_id:'$files_id'})")
+            rosaluxemburg4=$(mongo rosaluxemburg4:27018/$db --quiet --eval "db.$ns.chunks.count({files_id:'$files_id'})")
+            r="$db $ns $files_id $rosaluxemburg0 $rosaluxemburg2 $rosaluxemburg4"
+            echo $r
+            echo $r >> $chunks
         done
     done
 done
+
+
