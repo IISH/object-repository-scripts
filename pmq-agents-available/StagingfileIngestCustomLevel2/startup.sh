df81cd87a03e415809a129e509f9a8d03a2324dc
Lucien van Wouw <lwo@iisg.nl>
2012-10-17 00:52:14 +0200

    Add a new workflow for custom derivatives 2

---
+++ pmq-agents-available/StagingfileIngestCustomLevel2/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# StagingfileIngestCustomLevel2/startup.sh
+#
+# Ingests any custom derivatives of the level1, level2 and level3 sort.
+
+scripts=$scripts
+bucket="level2"
+source $scripts/shared/parameters.sh
+action=$action
+source $scripts/shared/primary.sh
+
+if [ "$action" == "delete" ] ; then
+    source $scripts/shared/delete.sh
+    exit $?
+fi
+
+source $scripts/shared/find.derivative.sh
+
+exit $?
