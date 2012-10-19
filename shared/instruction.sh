ce2a4efe0ba46150a374e1ba82557444f1bbceb5
Lucien van Wouw <lwo@iisg.nl>
2012-10-19 19:01:39 +0200

    The webservice key was not set when a profile delivers its own key.

--- shared/instruction.sh
+++ shared/instruction.sh
@@ -1,6 +1,14 @@
 #
 # Starts the instruction manager.
 #
+
+scripts=$scripts
+JAVA_OPTS=$JAVA_OPTS
+pidwebserviceEndpoint=$pidwebserviceEndpoint
+pidwebserviceKey=$pidwebserviceKey
+manager=$manager
+instruction=$instruction
+
 source $scripts/shared/parameters.sh $@
 
-java $JAVA_OPTS -Dmd5sum=md5sum -cp $manager "$instruction"
+java $JAVA_OPTS -DpidwebserviceEndpoint $pidwebserviceEndpoint -DpidwebserviceKey $pidwebserviceKey -Dmd5sum=md5sum -cp $manager "$instruction"
