adadf10a2a8e2bfc3da8514eab0f3646fdf441ab
Lucien van Wouw <lwo@iisg.nl>
2012-10-27 00:29:26 +0200

    Delete procedure called the identifier using quotes. This fill fail, but confirm a false removal.

--- shared/delete.sh
+++ shared/delete.sh
@@ -7,22 +7,22 @@ pid=$pid
 bucket=$bucket
 
     files_id=$(mongo $db --quiet --eval "var doc=db.$bucket.files.findOne({'metadata.pid':'$pid'}, {_id:1});if ( doc ){print(doc._id)}")
-    if [ -z $files_id ] ; then
+    if [ -z "$files_id" ] ; then
         echo "PID $pid not in database."
         exit 245
     fi
 
-    mongo $db --quiet --eval "db.$bucket.files.remove({_id:'$files_id'})"
-    mongo $db --quiet --eval "db.$bucket.chunks.remove({files_id:'$files_id'})"
+    mongo $db --quiet --eval "db.$bucket.files.remove({_id:$files_id})"
+    mongo $db --quiet --eval "db.$bucket.chunks.remove({files_id:$files_id})"
 
     # Verify our removal
-    count=$(mongo $db --quiet --eval "db.$bucket.files.count({_id:'$files_id'})")
+    count=$(mongo $db --quiet --eval "db.$bucket.files.count({_id:$files_id})")
     if [ $count != 0 ] ; then
         echo "Failed to delete document $pid in files.$bucket"
         exit -1
     fi
 
-    count=$(mongo $db --quiet --eval "db.$bucket.chunks.count({files_id:'$files_id'})")
+    count=$(mongo $db --quiet --eval "db.$bucket.chunks.count({files_id:$files_id})")
     if [ $count != 0 ] ; then
         echo "Failed to delete document $pid in chunks.$bucket"
         exit -1
