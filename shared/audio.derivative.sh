ca3d7002b76f472d23f605c8fa2fd79d5a901f8f
Lucien van Wouw <lwo@iisg.nl>
2012-10-26 13:34:51 +0200

    - empty audio and video conversion scripts

---
+++ shared/audio.derivative.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Runs Audio conversion software to convert an audio file into a ( smaller ) version.
+# Then ingests the derivative into the correct bucket.
+#
+
+derivative=$derivative
+
+echo "We cannot make a derivative of type $derivative"
+echo "This is not an error. Just there is no such service."
+exit 250
---
+++ shared/video.derivative.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Runs Video conversion software to convert a video into a ( smaller ) version.
+# Then ingests the derivative into the correct bucket.
+#
+
+derivative=$derivative
+
+echo "We cannot make a derivative of type $derivative"
+echo "This is not an error. Just there is no such service."
+exit 250
