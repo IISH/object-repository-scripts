8aa1a409f7a0d89b96481399ddc13b9c42848b76
Lucien van Wouw <lwo@iisg.nl>
2012-11-15 16:53:02 +0100

    We will accept a cached file if length is the same.

--- shared/get.bat
+++ shared/get.bat
@@ -8,13 +8,6 @@ IF "%pid%"=="" (
     exit -1
 )
 
-Rem We skip the get if a file with the same length is already here.
-if EXIST "%l%" (
-    call %scripts%\shared\set.bat length1 mongo %db% --quiet --eval "Number(db.%sourceBucket%.files.findOne({'metadata.pid':'%pid%'}).length)"
-    call %scripts%\shared\set.bat length2 php -r "print(filesize('%l%'));"
-    if %length1% == %length2% goto :EOF
-)
-
 echo "Get from db=%db% bucket=%sourceBucket% pid=%pid% to %l%"
 java -jar %orfiles% -M Get -l "%l%" -h %host% -d "%db%" -b %sourceBucket% -a %pid% -m ""
 
