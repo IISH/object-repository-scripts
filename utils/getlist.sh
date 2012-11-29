498a7f0665beff863628e39e80356b3703043c57
Lucien van Wouw <lwo@iisg.nl>
2012-11-29 22:30:57 +0100

    Utility scripts to aid chunk migration 2

--- utils/getlist.sh
+++ utils/getlist.sh
@@ -6,7 +6,12 @@ if [ -z "$limit" ] ; then
     exit -1
 fi
 
-mongo or_10622 --quiet --eval "db.master.files.find( {}, {_id:1, 'metadata.pid':1 } ).skip(0).limit(1000).forEach( \
+if ! [[ "$limit" =~ ^[-0-9]+$ ]] ; then
+    echo "$limit is not a number."
+    exit -1
+fi
+
+mongo or_10622 --quiet --eval "db.master.files.find( {}, {_id:1, 'metadata.pid':1 } ).skip($limit).limit(1000).forEach( \
     function(d){if(d._id.length>20)print('\$scripts/utils/change.key.sh '+d._id + ' ' + d.metadata.pid)});" > change.keys.$limit.sh
 
 chmod 744 change.keys.$limit.sh
