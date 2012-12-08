1a776b33aa6986846ba9fd073915121e80cb3643
Lucien van Wouw <lwo@iisg.nl>
2012-12-09 00:59:05 +0100

    Made so custom derivatives will always be accepted.

--- utils/getlist.sh
+++ utils/getlist.sh
@@ -7,11 +7,21 @@ if [ -z "$from" ] ; then
 fi
 
 if ! [[ "$from" =~ ^[-0-9]+$ ]] ; then
-    echo "$from is not a number."
+    echo "from $from is not a number."
     exit -1
 fi
 
-mongo or_10622 --quiet --eval "db.master.files.find( {}, {_id:1, 'metadata.pid':1 } ).skip($from).limit(1000).forEach( \
-    function(d){if(d._id.length>20)print('\$scripts/utils/change.key.sh '+d._id + ' ' + d.metadata.pid)});" > change.keys.$from.sh
+to=$2
+if [ -z "$to" ] ; then
+    echo Need a limit.
+    exit -1
+fi
 
-chmod 744 change.keys.$from.sh
+if ! [[ "$to" =~ ^[-0-9]+$ ]] ; then
+    echo "to $to is not a number."
+    exit -1
+fi
+
+file=change.keys.$from.$to.sh
+mongo or_10622 --quiet --eval "var i=0;db.master.files.find({},{_id:1,'metadata.pid':1}).forEach(function(d){if(d._id.length>20){i++;if(i>$from && i<$to) print('source \$scripts/utils/change.key.sh '+d._id+' '+d.metadata.pid)}})">$file
+chmod 744 $file
