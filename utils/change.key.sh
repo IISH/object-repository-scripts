edc20a9e1eb3ef5d275d6693772bcfcd36646b94
Lucien van Wouw <lwo@iisg.nl>
2012-12-15 09:53:18 +0100

    Check for secondary

--- utils/change.key.sh
+++ utils/change.key.sh
@@ -11,6 +11,15 @@ if [[ $new_id == 0 ]] ; then
     exit -1
 fi
 
+for host in rosaluxemburg1 rosaluxemburg7 rosaluxemburg5
+do
+    secondary=$(mongo $host:27018/test --quiet --eval "db.serverStatus().repl.secondary")
+    if [ ! "$secondary" == "true" ] ; then
+        echo "$host is not a secondary">>change.key.log
+        exit -1
+    fi
+done
+
 evl="var bucket='$bucket'; var old_id='$_id'; var new_id=$new_id; var pid='$pid';"
 echo $evl >> change.key.log
 mongo $db --quiet --eval "$evl" $scripts/utils/change.key.js >> change.key.log
