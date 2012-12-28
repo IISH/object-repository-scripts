464e51bf083b38af4e72d84c1df01ec0cf707e7c
Lucien van Wouw <lwo@iisg.nl>
2012-12-28 21:28:58 +0100

    Set the datestamp on accepted tasks.

--- pmq-agents-available/InstructionIngest/startup.sh
+++ pmq-agents-available/InstructionIngest/startup.sh
@@ -13,17 +13,18 @@ fi
 # Lets see if all stagingfiles have a default task 'Start'. In fact we look for alternative tasks who really should not
 # be there at all.
 fileSet=$fileSet
-count=$(mongo sa --quiet --eval "db.getCollection('stagingfile').find({fileSet:'$fileSet', \
-    \$or:[{'workflow.n':{\$exists:false}},{'workflow.n':0,'workflow.name':{\$ne:'Start'}}]}).count()")
-
-if [ $count != 0 ] ; then
-    echo "$count declared files do not have the expected 'Start' task: $fileSet"
-    exit -1
-fi
 
 # The application does not set the start and end dates correctly. Hence we set that here.
 mongo sa --quiet --eval "db.getCollection('stagingfile').update({fileSet:'$fileSet', 'workflow.name':'Start'}, \
     {\$set:{'workflow.\$.start':new Date(), 'workflow.\$.end':new Date()}}, \
     false, true)"
+
+count=$(mongo sa --quiet --eval "db.getCollection('stagingfile').find({fileSet:'$fileSet', \
+    \$or:[{'workflow.n':{\$exists:false}},{'workflow.n':0,'workflow.name':{\$ne:'Start'}}]}).count()")
+if [ $count != 0 ] ; then
+    echo "$count declared files do not have the expected 'Start' task: $fileSet"
+    exit -1
+fi
+
 
 exit 0
