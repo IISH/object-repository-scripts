f256f66fde47639c246e4efbf9ec58d770879e4d
Lucien van Wouw <lwo@iisg.nl>
2013-03-13 23:07:19 +0100

    Setting a variable inside a if statement does not work... moving it outside.

--- shared/hasdocument.bat
+++ shared/hasdocument.bat
@@ -1,7 +1,8 @@
-if "%$replaceExistingDerivatives%" == "true" (
+call %scripts%\shared\set.bat hasdocument mongo %db% --quiet --eval "var bucket='%bucket%';var pid='%pid%'" %scripts%\shared\hasdocument.js
+
+if "%replaceExistingDerivatives%" == "true" (
     echo Replace existing derivatives
 ) else (
-    call %scripts%\shared\set.bat hasdocument mongo %db% --quiet --eval "var bucket='%bucket%';var pid='%pid%'" %scripts%\shared\hasdocument.js
     if "%hasdocument%" == "true" (
         echo "The file in %bucket% with %pid% exists. Hence we stop processing here."
         exit 245
