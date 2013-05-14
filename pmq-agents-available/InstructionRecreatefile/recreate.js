d30bfd498fe7558fe5cd6f98cb6610baa5079cfb
Lucien van Wouw <lwo@iisg.nl>
2013-05-14 16:32:52 +0200

    Add recreate instruction file task 2

--- pmq-agents-available/InstructionRecreatefile/recreate.js
+++ pmq-agents-available/InstructionRecreatefile/recreate.js
@@ -1,14 +1,40 @@
 /**
  recreate.js
 
- Take all metadata from a single file into a instruction
+ Take all metadata from the master files into a instruction
  **/
 
 assert(db.getName() != 'test', "The database is the test database. Startup specifying a production database: 'mongo host/database'");
+assert(na, "Need a naming authority");
 assert(id, "Must have a id: var id='?'");
 
 var sa = db.getMongo().getDB("sa");
 var instruction = sa.instruction.findOne(ObjectId(id));
 assert(instruction, "The instruction is absent and must be created first.");
 assert(instruction.fileSet, "The instruction has not fileSet.");
-assert(instruction.label, "The instruction has no label.");
+assert(instruction.workflow.size() !=0, "The instruction has no workflow element.");
+assert(instruction.workflow[1].info, "The instruction has no info.");
+var pid = instruction.workflow[1].info ;
+
+db.master.files.find({'metadata.pid':pid}, {'metadata.content':0}).forEach(function (d) {
+    var document = {
+        na:na,
+        access:d.access,
+        contentType:d.contentType,
+        md5:d.md5,
+        length:d.length,
+        pid:d.metadata.pid,
+        seq:d.metadata.seq,
+        objid:d.metadata.objid,
+        fileSet:d.metadata.fileSet,
+        version:NumberLong(0),
+        _class:'org.objectrepository.instruction.StagingfileType'
+    };
+    if (instruction.access == document.access) delete document.access;
+    if (instruction.contentType == document.contentType) delete document.contentType;
+    if (instruction.objid == document.objid) delete document.objid;
+    document.location = d.metadata.l + '/' + d.filename;
+    if (d.metadata.lid) document.lid = d.metadata.lid;
+
+    sa.stagingfile.save(document);
+});
--- pmq-agents-available/InstructionRecreatefile/startup.sh
+++ pmq-agents-available/InstructionRecreatefile/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# /InstructionRecreatefile/startup.sh
+# /InstructionRecreate/startup.sh
 #
-# Creating a fileset based on an existing instruction
+# Creating an instruction based on a pid value in the workflow task info field.
 
 scripts=$scripts
 source $scripts/shared/parameters.sh
@@ -11,7 +11,7 @@ na=$na
 fileSet=$fileSet
 id=$id
 
-mongo $db --quiet --eval "var id='$id';" $scripts/pmq-agents-available/InstructionRecreatefile/recreate.js
+mongo $db --quiet --eval "var na='$na'; var id='$id';" $scripts/pmq-agents-available/InstructionRecreatefile/recreate.js
 
 rc=$?
 if [[ $rc != 0 ]] ; then
