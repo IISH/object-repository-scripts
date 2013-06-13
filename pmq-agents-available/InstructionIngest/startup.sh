1939371fe0f8a034778906ce935d07156917648f
Lucien van Wouw <lwo@iisg.nl>
2013-06-14 00:48:33 +0200

    Simplify the ingest call

--- pmq-agents-available/InstructionIngest/startup.sh
+++ pmq-agents-available/InstructionIngest/startup.sh
@@ -2,34 +2,22 @@
 #
 # /InstructionIngest/startup.sh
 
-scripts=$scripts
-source $scripts/shared/instruction.sh
-
-rc=$?
-if [[ $rc != 0 ]] ; then
-    mongo sa --quiet --eval "db.getCollection('stagingfile').update({fileSet:'$fileSet'}, \
-        {\$unset:{workflow:1}}, false, true)"
-    exit $rc
-fi
-
-# Lets see if all stagingfiles have a default task 'Start'.
-# In fact we look for alternative tasks who really should not be there at all.
-fileSet=$fileSet
-count=$(mongo sa --quiet --eval "db.getCollection('stagingfile').find({fileSet:'$fileSet', \
-    \$or:[{'workflow.n':{\$exists:false}},{'workflow.n':0,'workflow.name':{\$ne:'Start'}}]}).count()")
+q="fileSet:'$fileSet'"
+count=$(mongo sa --quiet --eval "db.stagingfile.count({$q, workflow:{\$elemMatch:{n:0, name:'InstructionValidated', statusCode:{\$lt:800}}}})")
 if [[ $count != 0 ]] ; then
-    echo "$count declared files do not have the expected 'Start' task: $fileSet"
-    echo "Undo any tasks"
-    mongo sa --quiet --eval "db.getCollection('stagingfile').update({fileSet:'$fileSet'}, \
-        {\$unset:{workflow:1}}, false, true)"
+    echo "$count staging file elements did not passed validation. Correct the problems first before starting the ingest."
     exit -1
 fi
 
-# The application does not set the start and end dates correctly. Hence we set that here.
+# Set the start and end dates correctly. Hence we set that here.
 # We set the end date in the past so the workflow controller will pick up on it.
-lasterror=$(mongo sa --quiet --eval "db.getCollection('stagingfile').update({fileSet:'$fileSet', 'workflow.name':'Start'}, \
-    {\$set:{'workflow.\$.start':new Date(), 'workflow.\$.end':new Date(0)}}, \
-    false, true);db.runCommand({getlasterror:1, w:'majority'}).err")
+lasterror=$(mongo sa --quiet --eval "db.stagingfile.update({$q}, \
+    {\$set:{workflow:[{ \
+        n:0, name:'Start', statusCode:100, start:new Date(), end:new Date(0), total:0, processed:0, \
+        attempts:1, limit:3, exitValue:2147483647, info:'Starting InstructionIngest' \
+        }]}}, \
+        false, true); \
+        db.runCommand({getlasterror:1, w:'majority'}).err")
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo $lasterror
