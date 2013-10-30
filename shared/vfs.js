c7fcea8574e69dc2d9a9354a45d0928f2cae5b29
Lucien van Wouw <lwo@iisg.nl>
2013-10-30 21:03:39 +0100

    Make sure when we find a parent, it has directories.

--- shared/vfs.js
+++ shared/vfs.js
@@ -67,16 +67,18 @@ db.getCollection(ns + '.files').find(
                     var _f = {n: d.filename, p: d.metadata.pid, l: d.length, t: d.uploadDate.getTime(), a: d.metadata.access};
                     if (d.metadata.objid) _f.o = d.metadata.objid;
                     db.vfs.update({_id: child}, {$addToSet: {f: _f}}, true, false);
+                    assert(db.runCommand({getlasterror: 1, w: "majority"}).err == null, "Could not update vfs.");
                 }
 
                 // folder
                 var doc = db.vfs.findOne({_id: parent});
-                if (doc) {
+                if (doc && doc.d) {
                     for (D = 0; D <= doc.d.length; D++) {
                         if (D == doc.d.length) {
                             if (log) print(count + ' new sub directory ' + n + ' in parent ' + parent + ' in ' + l);
                             doc.d.push(update({}, n, d.metadata.access, d.uploadDate.getTime()));
                             db.vfs.save(doc);
+                            assert(db.runCommand({getlasterror: 1, w: "majority"}).err == null, "Could not update vfs.");
                             break;
                         }
                         else if (doc.d[D].n == n) {
@@ -84,6 +86,7 @@ db.getCollection(ns + '.files').find(
                                 update(doc.d[D], n, d.metadata.access, d.uploadDate.getTime());
                                 if (log) print(count + ' existing sub directory ' + n + ' in parent ' + parent + ' in ' + l);
                                 db.vfs.save(doc);
+                                assert(db.runCommand({getlasterror: 1, w: "majority"}).err == null, "Could not update vfs.");
                             }
                             break;
                         }
@@ -94,6 +97,7 @@ db.getCollection(ns + '.files').find(
                     db.vfs.save({_id: parent, d: [
                         update({}, n, d.metadata.access, d.uploadDate.getTime())
                     ]});
+                    assert(db.runCommand({getlasterror: 1, w: "majority"}).err == null, "Could not update vfs.");
                 }
             }
         }
