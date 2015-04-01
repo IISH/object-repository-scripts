a42c9ad6f5835a3fcf4c8d9058ec37012e31e6cc
Lucien van Wouw <lwo@iisg.nl>
2015-04-01 23:54:00 +0200

    Read in environmental variables

--- pmq-agents-available/StagingfileIngestLevel1Audio/startup.bat
+++ pmq-agents-available/StagingfileIngestLevel1Audio/startup.bat
@@ -3,4 +3,4 @@ Rem
 Rem Starts the bash convert script to create the level 1 derivative for a Video.
 
 cd %CYGWIN_HOME%\bin
-bash.exe -l -c "%scripts%/pmq-agents-available/StagingfileIngestLevel1Audio/startup.sh %*"
+bash.exe -l -c '%scripts%/pmq-agents-available/StagingfileIngestLevel1Audio/startup.sh %*'
