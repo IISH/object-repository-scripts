439204deac414ef83f5bf2af87f8711df55326b2
Lucien van Wouw <lwo@iisg.nl>
2015-04-01 18:42:23 +0200

    Read in environmental variables

--- pmq-agents-available/StagingfileIngestLevel1Audio/startup.bat
+++ pmq-agents-available/StagingfileIngestLevel1Audio/startup.bat
@@ -1,21 +1,6 @@
 Rem /StagingfileIngestLevel1Audio/startup.bat
 Rem
-Rem The convert script to create the level 1 derivative for an Audio
+Rem Starts the bash convert script to create the level 1 derivative for a Video.
 
-set scripts=%scripts%
-call %scripts%\shared\parameters.bat %*
-
-set sourceBucket=master
-set bucket=level1
-
-call %scripts%\shared\delete.bat
-call %scripts%\shared\hasdocument.bat
-
-Rem Preset 120 = Audio Only: MP3 High Quality
-set preset=120
-set targetContentType=audio/mp3
-set derivative=audio
-set format=mp3
-
-call %scripts%\shared\av.derivative.bat
-exit %errorlevel%
+cd %CYGWIN_HOME%\bin
+bash.exe -l -c "%scripts%/pmq-agents-available/StagingfileIngestLevel1Image/startup.sh"
--- pmq-agents-available/StagingfileIngestLevel1Video/startup.bat
+++ pmq-agents-available/StagingfileIngestLevel1Video/startup.bat
@@ -2,20 +2,5 @@ Rem /StagingfileIngestLevel1Video/startup.bat
 Rem
 Rem The convert script to create the level 1 derivative for a Video
 
-set scripts=%scripts%
-call %scripts%\shared\parameters.bat %*
-
-set sourceBucket=master
-set bucket=level1
-
-call %scripts%\shared\delete.bat
-call %scripts%\shared\hasdocument.bat
-
-Rem Preset 4169 = H.264 AAC ; same framerate as input
-set preset=4169
-set targetContentType=video/mp4
-set derivative=video
-set format=mp4
-
-call %scripts%\shared\av.derivative.bat
-exit %errorlevel%
+cd %CYGWIN_HOME%\bin
+bash.exe -l -c "%scripts%/pmq-agents-available/StagingfileIngestLevel1Image/startup.sh"
