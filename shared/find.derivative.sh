8c31848444bc4b82ece218645038a8c180532ad5
lwo <lwo@iisg.nl>
2016-04-12 09:38:56 +0200

    SOR-387: Space in filename splits the argument value if not quoted.

--- shared/find.derivative.sh
+++ shared/find.derivative.sh
@@ -16,7 +16,7 @@ echo "Check for existing derivative on fs for master $location"
 sourceFile=$(php $scripts/shared/find.derivative.php -f "$fileSet" -l "$location" -b ".$bucket")
 if [ -f "$sourceFile" ]; then
     echo "Found custom file: $sourceFile"
-    contentType=$(php $scripts/shared/extension2contenttype.php -t $scripts/shared/contenttype.txt -l $sourceFile)
+    contentType=$(php $scripts/shared/extension2contenttype.php -t $scripts/shared/contenttype.txt -l "$sourceFile")
     l=$sourceFile
     md5=$(md5sum "$l" | cut -d ' ' -f 1)
     remove="yes"
