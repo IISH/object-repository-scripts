2de3c2f749f3516397ea8d47077a24c98e40200b
lwo <lwo@iisg.nl>
2016-06-02 10:59:43 +0200

    Prepare rar procedure.

--- pmq-agents-available/InstructionPackage/startup.sh
+++ pmq-agents-available/InstructionPackage/startup.sh
@@ -4,84 +4,92 @@
 #
 # Build a rar package and a whole new instruction around it.
 #
-# We run three commands with options:
-#
-# Command 1:         create the archive.
-# rar a              Add files to archive.
-#   -m0              Store files. So no compression.
-#   -rr5%            Optionally, redundant information (recovery record) may be added to an archive, incurring a small
-#                        penalty in archive size, which will help to recover archived files in the case of a diskette
-#                        failure or other data losses.
-#   -v107374182400b  100GiB multipart package.
-#   -x*.md5          Ignore files with a .md5 postfix.
-#   -r               Recurse subdirectories.
-#
-# Command 2:         Test the archive
-# rar t -r '*'
-#
-# Command 2:         lock the archive.
-# rar k              Lock archive. Any command which intend to change the archive will be ignored.
+# We run the rar command with these options:
+# $ rar a                  Create and add files to archive.
+#       -ep1               Exclude base directory from names.
+#       -k                 Lock the archive.
+#       -m0                Store files. So no compression.
+#       -r                 Recurse
+#       -rr5%              Optionally, redundant information (recovery record) may be added to an archive, incurring a small
+#                                penalty in archive size, which will help to recover archived files in the case of a diskette
+#                               failure or other data losses.
+#       -t                 Test the archive
+#       -v107374182400b    100GiB multipart package.
+#       -x*.md5            Ignore files with a .md5 postfix.
+
 
 scripts=$scripts
 source $scripts/shared/parameters.sh
+access=$access
 fileSet=$fileSet
 archiveID=$(basename "$fileSet")
 workdir="$(dirname "$fileSet")/.work"
 archive="${workdir}/${archiveID}.rar"
+expected_archive="${fileSet}/${archiveID}.rar"
+expected_archive_part="${fileSet}/${archiveID}.part01.rar"
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-# Remove stale working.
+# Check if previous backups of the fileset are there.
 #-----------------------------------------------------------------------------------------------------------------------
-if [ -d "$workdir" ]
-then
-    echo "Found working directory. Deleting: ${workdir}"
-    rm -rf "$workdir"
-fi
+rc=0
+for test_archive in "$expected_archive" "$expected_archive_part"
+do
+    rar t -r "$test_archive"
+    rc=$?
+    if [[ $rc == 0 ]] ; then
+        echo "Found a valid archive: ${test_archive}"
+        break
+    fi
+done
 
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Create the archive.
-#-----------------------------------------------------------------------------------------------------------------------
-rar a -m0 -r -rr5% -v107374182400b "$archive" "$fileSet"
-rc=$?
 if [[ $rc != 0 ]] ; then
-    echo "rar 'a' command returned an error ${rc}"
-    exit 1
+
+    #-----------------------------------------------------------------------------------------------------------------------
+    # Remove stale working directory.
+    #-----------------------------------------------------------------------------------------------------------------------
+    if [ -d "$workdir" ]
+    then
+        echo "Found working directory. Deleting: ${workdir}"
+        rm -rf "$workdir"
+    fi
+    mkdir -p "$workdir"
+
+
+    #-----------------------------------------------------------------------------------------------------------------------
+    # Create a multipart archive of 100GiB.
+    #-----------------------------------------------------------------------------------------------------------------------
+    rar a -ep1 -k -m0 -r -rr5% -t -v107374182400b -x*.md5 "$archive" "$fileSet"
+    rc=$?
+    if [[ $rc == 0 ]] ; then
+        echo "As we made a rar archive and validated it, we now remove the original SIP."
+        perl -e "use File::Path;\$count=rmtree('/tmp/a');print('Removed ' . \$count);"
+    else
+        echo "rar 'a' command returned an error ${rc}"
+        rm "$archive"
+        exit 1
+    fi
 fi
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-# Lock archive.
+# We have a valid archive. Create the SIP
 #-----------------------------------------------------------------------------------------------------------------------
-rar r "$archive" "$fileSet"
-if [[ $rc != 0 ]] ; then
-    echo "rar 'r' command returned an error ${rc}"
-    exit 1
-fi
+instruction="$fileSet/instruction.xml"
+echo "<instruction>" >> "$instruction"
+
+for rar_part in "$workdir"
+do
+    l="${rar_part}.md5"
+    md5sum "$rar_part" > "$l"
+    md5=$(cat "$l" | cut -d ' ' -f 1)
+    location="${fileset}$(basename "$rar_part")"
+    echo "<stagingfile>" >> "$instruction"
+    echo "</stagingfile>" >> "$instruction"
+done
+echo "</instruction>" >> "$instruction"
 
 
-#-----------------------------------------------------------------------------------------------------------------------
-# Test archive.
-#-----------------------------------------------------------------------------------------------------------------------
-rar t -r '*' "$archive" "$fileSet"
-rc=$?
-if [[ $rc != 0 ]] ; then
-    echo "rar 't' command returned an error ${rc}"
-    exit 1
-fi
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Create the SIP
-#-----------------------------------------------------------------------------------------------------------------------
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Move the fileSet to the working directory.
-#-----------------------------------------------------------------------------------------------------------------------
-mv "$fileSet" "$workdir"
 
 
 
