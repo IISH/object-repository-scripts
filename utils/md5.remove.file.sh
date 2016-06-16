21655aa1f6dc1fe79870f40bc931272bc3226855
lwo <lwo@iisg.nl>
2016-06-16 18:14:49 +0200

    Add utility for the resubmission procedure.

--- utils/md5.remove.file.sh
+++ utils/md5.remove.file.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#
+# Remove a file when it ends with .md5
 
-f=$1
+f="$1"
 
-if [ -f $f ] ; then
-    if [[ "$f" == *md5 ]]
+if [ -f "$f" ] ; then
+    if [[ "$f" == *.md5 ]]
     then
         echo "Remove md5 file ${f}"
         rm -f "$f"
--- utils/resubmit.sh
+++ utils/resubmit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
+# resubmit.sh [objid]
+#
+# example: resubmit.sh 10622/BULK12345
 
 objid="$1"
 if [ -z "$objid" ]
@@ -14,12 +17,17 @@ id=${objid:6}
 if [ ! "$objid" == "${na}/${id}" ]
 then
     echo "The expected na and id are not correctly derived from the objid."
+    echo "Eg: ./resubmit.sh 10622/BULK12345"
     exit 2
 fi
 
-source $scripts/utils/delete_by_objid.sh "$na" "$objid"
+$scripts/utils/delete_by_objid.sh "$objid"
 
-d="/mnt/sa/10622/23445/$1"
-if [ -d "$d" ] ; then
-    find "$d" -type f -exec $scripts/utils/md5.remove.file.sh {} \;
-fi
+fileset="/mnt/sa/10622/23445/${id}"
+backup="/mnt/sa/10622/23445/.${id}"
+# remove the old instruction
+query="{fileSet:'${fileset}'}"
+mongo sa --quiet --eval "db.instruction.remove($query)"
+mongo sa --quiet --eval "db.stagingfile.remove($query)"
+
+mv "$fileset" "$backup"
