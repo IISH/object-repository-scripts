0436bb82e5f1f3956804ecb5acbc8c9ec6362185
lwo <lwo@iisg.nl>
2016-09-22 16:06:03 +0200

    Delete files from the database when their checksum value does not match.

--- shared/put.sh
+++ shared/put.sh
@@ -155,13 +155,16 @@ then
     md5_check_file="${workdir}/${md5}.bin"
     java -jar "$orfiles" -M Get -l "$md5_check_file" -host "$host" -d "$db" -b master -a "$pid" -m ""
     md5_check=$(md5sum "$md5_check_file" | cut -d ' ' -f 1)
+    rm "$md5_check_file"
     if [ "$md5" == "$md5_check" ]
     then
         echo "md5 checksum ok"
-        rm "$md5_check_file"
     else
         echo "md5 mismatch then comparing the file with a checkout version."
         echo "Expect ${md5} but got ${md5_check}  ${md5_check_file}"
+        mongo "$db" --quiet --eval "db.$bucket.files.remove({_id:$shardKey})"
+        mongo "$db" --quiet --eval "db.$bucket.chunks.remove({files_id:$shardKey})"
+        mongo "$db" --quiet --eval "var pid='$pid';var ns='$bucket'; var del=true;" $(cwp "$scripts/shared/vfs.js")
         exit 1
     fi
 fi
@@ -178,8 +181,9 @@ then
             echo "No derivative in level ${b} found."
         else
             echo "Remove derivative ${b}"
-            mongo $db --quiet --eval "db.$b.files.remove({_id:$files_id})"
-            mongo $db --quiet --eval "db.$b.chunks.remove({files_id:$files_id})"
+            mongo "$db" --quiet --eval "db.$b.files.remove({_id:$files_id})"
+            mongo "$db" --quiet --eval "db.$b.chunks.remove({files_id:$files_id})"
+            mongo "$db" --quiet --eval "var pid='$pid';var ns='$bucket'; var del=true;" $(cwp "$scripts/shared/vfs.js")
         fi
     done
 fi
