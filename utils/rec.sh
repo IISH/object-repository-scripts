4c247f7bc650504bd5dc2721a55b19789993292f
lwo <lwo@iisg.nl>
2016-12-22 13:52:19 +0100

    Delete by objid and make sure we remove all derivates in all buckets.

--- utils/rec.sh
+++ utils/rec.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# mongo $2 --quiet --eval "db.level3.files.find({'metadata.pid':{\$regex:'^10622\/30051'}},{_id:0,'metadata.pid':1}).limit(90).forEach(function(d){print(d.metadata.pid)})" > barcodes.txt
 
-echo "<instruction xmlns='http://objectrepository.org/instruction/1.0/' autoIngestValidInstruction='true' action='upsert' plan='StagingfileIngestLevel3'>"
+echo "<instruction label='barcode grotere lengte x breedte' xmlns='http://objectrepository.org/instruction/1.0/' autoIngestValidInstruction='true' action='upsert' plan='StagingfileIngestLevel3'>"
 while read pid
 do
     echo "<stagingfile><pid>${pid}</pid></stagingfile>"
