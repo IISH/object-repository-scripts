7576a6bb0b9a099316caeee93f1369260c900221
lwo <lwo@iisg.nl>
2017-02-17 13:57:01 +0100

    Empty the cache folder if it is lower than the limit.

--- utils/derivative_cache_empty.sh
+++ utils/derivative_cache_empty.sh
@@ -4,7 +4,7 @@
 LIMIT=20
 
 count=$(ls -al "${MAGICK_TMPDIR}" | wc -l)
-if [[ $count > $LIMIT ]]
+if [[ $count -gt $LIMIT ]]
 then
    echo "Emptying cache ${MAGICK_TMPDIR}"
    rm -f "${MAGICK_TMPDIR}/*"
